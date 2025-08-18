b3884ddbc22498e4d31264af6edd1b4260a88aa1
Nasaee <nasaee.dev@gmail.com>
2025-08-19 01:52:23 +0700

    setup oneentry

--- actions/auth/retrieveRefreshToken.ts
+++ actions/auth/retrieveRefreshToken.ts
@@ -2,7 +2,7 @@
 
 import { cookies } from 'next/headers';
 
-export default async function retrieveRefreshToken(token: string) {
+export default async function retrieveRefreshToken() {
   const cookieStore = await cookies();
   return cookieStore.get('refresh_token')?.value;
 }
